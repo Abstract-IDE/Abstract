d7035faa1fabbff60ed883a50e90fac49a8afe3b
Already told <icanttell@gmail.com>
2021-10-24 09:29:50 +0545

    override_defalut.lua, now we can override any defined config by defining in override_defalut.lua

---
+++ lua/customs/override_defalut.lua
@@ -0,0 +1,73 @@
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--─────────────────────────────────────────────────--
+--          Override configs
+-- this config file is used to override the already defined
+-- config/mappings. This prevent config more readable and less complicated.
+
+--─────────────────────────────────────────────────--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+
+
+
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━❰ configs ❱━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+--[===[         Examples for Config
+
+local exec  = vim.api.nvim_exec -- execute Vimscript
+local opt   = vim.opt           -- global options
+local cmd   = vim.cmd           -- execute Vim commands
+local fn    = vim.fn            -- call Vim functions
+local g     = vim.g             -- global variables
+local b     = vim.bo            -- buffer-scoped options
+local w     = vim.wo            -- windows-scoped options
+
+-- don't automatically wrap on load
+opt.wrap = false
+
+-- highlight on yank
+exec([[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=500, on_visual=true}
+  augroup end
+]], false)
+
+-- 4 spaces for selected filetypes
+cmd([[ autocmd FileType python,c,cpp setlocal shiftwidth=4 tabstop=4 ]])
+
+]===]
+
+
+
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+
+
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━❰ Mappings ❱━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+--[===[         Examples for Config
+local keymap = vim.api.nvim_set_keymap
+
+-- to save file
+keymap('i', '<C-s>',    '<ESC>:w <CR>', { noremap=true, silent=true })
+
+]===]
+
+
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━❰ end Mappings ❱━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+
+
