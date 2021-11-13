7f0330258752f2868b3ced077489fcc309f963af
Already told <icanttell@gmail.com>
2021-11-13 18:14:10 +0545

    .__roshnivim__ file is use indicate the roshnivim project

--- lua/plugins/rooter_nvim.lua
+++ lua/plugins/rooter_nvim.lua
@@ -4,7 +4,7 @@
 
 
 -- List of patterns(file or folder) which describes a directory as project root	li
-vim.g.rooter_pattern = { '.__project__',  '.git', 'Makefile', '_darcs', '.hg', '.bzr', '.svn', 'node_modules', 'CMakeLists.txt' }
+vim.g.rooter_pattern = { '.__roshnivim__', '.__project__',  '.git', 'Makefile', '_darcs', '.hg', '.bzr', '.svn', 'node_modules', 'CMakeLists.txt' }
 
 
 -- Whether to change directory to outermost root directory or not
