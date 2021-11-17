0ae8b7874a4ec7ce6ad2a9076944e6a0ff98e08b
Already told <icanttell@gmail.com>
2021-11-17 22:34:57 +0545

    improve code format

--- setup.py
+++ setup.py
@@ -1,39 +1,40 @@
 # importing required module
-#-------------------------------
+# -------------------------------
 import subprocess
 import argparse
 from pathlib import Path
 from datetime import datetime
 import fileinput
-#-------------------------------
+# -------------------------------
+
 
 # argument handeling
-#-------------------------------
+# -------------------------------
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--delete",
     "-d",
     type=int,
     default=0,
-    help=
-    "0 or 1, 0 means don't delete(Default) and 1 means delete the no required file/directory (.git, LICENSE, README.md)",
+    help="0 or 1, 0 means don't delete(Default) and 1 means delete the no \
+            required file/directory (.git, LICENSE, README.md)",
 )
 parser.add_argument(
     "--backup",
     "-b",
     type=int,
     default=1,
-    help=
-    "0 or 1, 0 means don't backup and 1 means backup(Default) the the config before any configuration",
+    help="0 or 1, 0 means don't backup and 1 means backup(Default) the the \
+            config before any configuration",
 )
 
 args = parser.parse_args()
 delete = args.delete
 backup = args.backup
-#-------------------------------
+# -------------------------------
 
 # directory locations
-#-------------------------------
+# -------------------------------
 HOME = Path.home()
 CACHE = f"{HOME}/.cache"
 CONFIG = f"{HOME}/.config"
@@ -42,10 +43,10 @@ NVIM_CONF_PATH = f"{CONFIG}/nvim"
 NVIM_PLUG_PATH = f"{HOME}/.local/share/nvim"
 CUSTOM_TOOLS_DIR = f"{NVIM_PLUG_PATH}/custom_tools"
 
-#-------------------------------
+# -------------------------------
 
 # directories we must have
-#-------------------------------
+# -------------------------------
 require_dir = [
     f"{CONFIG}",
     f"{CACHE}/nvim/swap",
@@ -56,40 +57,37 @@ require_dir = [
     CACHE_BUILD_PATH,
 ]
 
-#-------------------------------
+# -------------------------------
 
 
-#-------------------------------
+# -------------------------------
 def create_require_dir(dirs):
     for dir in dirs:
         if not Path(dir).exists():
             Path(dir).mkdir(parents=True)
             print(dir)
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def clone_repro(path, repository):
     subprocess.run(['git', 'clone', repository], cwd=path)
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def backup_nvim():
     current_date = datetime.today().strftime("%Y-%m-%d_%T")
     if Path(NVIM_CONF_PATH).exists():
         subprocess.run(['mv', 'nvim', f'nvim-old_{current_date}'], cwd=CONFIG)
         print(f"your old config: {NVIM_CONF_PATH}_{current_date}")
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def roshnivim_exist():
     """check if roshnivim already exist"""
 
@@ -98,22 +96,20 @@ def roshnivim_exist():
             return True
     return False
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def roshnivim_git():
     """check if roshnivim is git project"""
     if Path(f"{NVIM_CONF_PATH}/.git").exists():
         return True
     return False
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def need_to_clone_roshnivim():
     """check if we need to clone the roshnivim repro,
     if setup.py run without cloning roshnivim
@@ -124,30 +120,27 @@ def need_to_clone_roshnivim():
         return False
     return True
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def replace_text(filename, old_text, new_text):
     with fileinput.FileInput(filename, inplace=True) as file:
         for line in file:
             print(line.replace(old_text, new_text), end='')
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def compile_nvim():
     packer_compile_cmd = ["nvim", "--headless", "-c", "PackerSync"]
     subprocess.run(packer_compile_cmd, cwd=NVIM_CONF_PATH)
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def remove_no_require():
     subprocess.run(["rm", "-rf", ".git", "LICENSE", "README.md", "setup.py"],
                    cwd=NVIM_CONF_PATH)
@@ -156,11 +149,10 @@ def remove_no_require():
     print("Removed: README.md")
     print("Removed: setup.py")
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def install_roshnivim():
 
     text_colorscheme = "cmd('colorscheme rvcs'"
@@ -182,10 +174,10 @@ def install_roshnivim():
     ]
     try:
         subprocess.run(packer_install_cmd, cwd=NVIM_CONF_PATH)
-    except:
+    except KeyboardInterrupt:
         print("\n")
 
-    # going to uncomment the line of code we commented before installing the plugin
+    # uncomment the line of code we commented before installing the plugin
     replace_text(f"{NVIM_CONF_PATH}/lua/configs.lua", f"--{text_colorscheme}",
                  text_colorscheme)
     replace_text(f"{NVIM_CONF_PATH}/init.lua", f"--{text_impatient}",
@@ -197,17 +189,17 @@ def install_roshnivim():
     print("\n\nwow! roshnivim is installed")
     try:
         print(
-            "\nwhen you see something like: 'packer.compile: Complete',\npress CTRL+C\n"
+            "\nwhen you see something like: 'packer.compile: Complete',\
+                    \npress CTRL+C\n"
         )
         compile_nvim()
     except KeyboardInterrupt:
         print("\n")
 
+# -------------------------------
 
-#-------------------------------
 
-
-#-------------------------------
+# -------------------------------
 def main():
 
     print("creating required directories...")
@@ -218,7 +210,8 @@ def main():
         subprocess.run(["git", "pull"], cwd=NVIM_CONF_PATH)
         try:
             print(
-                "\nwhen you see something like: 'packer.compile: Complete'\n, press CTRL+C\n"
+                "\nwhen you see something like: 'packer.compile: Complete'\n, \
+                        press CTRL+C\n"
             )
             compile_nvim()
         except KeyboardInterrupt:
@@ -229,24 +222,26 @@ def main():
             backup_nvim()
         if need_to_clone_roshnivim():
             roshnivim_link = "https://github.com/shaeinst/roshnivim"
-            subprocess.run(["git", "clone", roshnivim_link, "nvim"], cwd=CONFIG)
+            subprocess.run(
+                ["git", "clone", roshnivim_link, "nvim"], cwd=CONFIG
+            )
         else:
             print("copying config...")
             subprocess.run(["cp", "-r", "../roshnivim", f"{CONFIG}/nvim"])
         install_roshnivim()
 
     # cloning lazy-builder tool (https://github.com/shaeinst/lazy-builder)
-    lazy_builder_path = f"{NVIM_PLUG_PATH}/custom_tools/lazy-builder"
+    lazy_builder_path = f"{CUSTOM_TOOLS_DIR}/lazy-builder"
     if not Path(lazy_builder_path).exists():
         print("installing additional...")
         repository = "https://github.com/shaeinst/lazy-builder"
-        clone_repro(lazy_builder_path, repository)
+        clone_repro(CUSTOM_TOOLS_DIR, repository)
 
     if delete == 1:
         print("cleaning config...")
         remove_no_require()
 
+# -------------------------------
 
-#-------------------------------
 
 main()
