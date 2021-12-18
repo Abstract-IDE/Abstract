6852d27077f7e0c782635d0ea00e6f7a39089eb5
Already told <icanttell@gmail.com>
2021-12-18 23:01:25 +0545

    add plugin config: alpha-nvim

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,60 @@
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--─────────────────────────────────────────────────--
+--   Plugin:    alpha-nvim
+--   Github:    github.com/goolord/alpha-nvim
+--─────────────────────────────────────────────────--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+
+
+
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━❰ configs ❱━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+local function footer()
+  local datetime = os.date("%I:%M:%p | %d-%m-%Y")
+  return datetime
+end
+
+
+-- Set header
+dashboard.section.header.val = {
+"																			",
+"	██████╗  ██████╗ ███████╗██╗  ██╗███╗   ██╗██╗██╗   ██╗██╗███╗   ███╗	",
+"	██╔══██╗██╔═══██╗██╔════╝██║  ██║████╗  ██║██║██║   ██║██║████╗ ████║	",
+"	██████╔╝██║   ██║███████╗███████║██╔██╗ ██║██║██║   ██║██║██╔████╔██║	",
+"	██╔══██╗██║   ██║╚════██║██╔══██║██║╚██╗██║██║╚██╗ ██╔╝██║██║╚██╔╝██║	",
+"	██║  ██║╚██████╔╝███████║██║  ██║██║ ╚████║██║ ╚████╔╝ ██║██║ ╚═╝ ██║	",
+"	╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝	",
+"																			",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "e", "  > New file" ,	":ene <BAR> startinsert <CR>"),
+    dashboard.button( "r", "  > Recent"   ,	":Telescope oldfiles<CR>"),
+	dashboard.button( "t", "  > Find text",	":Telescope live_grep <CR>"),
+    dashboard.button( "f", "  > Find file",	":Telescope find_files<CR>"),
+    dashboard.button( "s", "  > Settings" ,	":e ~/.config/nvim/init.lua <CR>"),
+    dashboard.button( "q", "  > Quit NVIM",	":qa<CR>"),
+}
+
+dashboard.section.footer.val = footer()
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
+
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━--
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+
