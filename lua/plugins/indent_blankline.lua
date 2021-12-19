8061a2adb5677feb0ca0d36e35c4b0a666611478
Already told <icanttell@gmail.com>
2021-12-19 13:01:18 +0545

    indent_blankline.lua: set trailing_blankline_indent to false

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -15,10 +15,20 @@ require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
+
+    -- char_list = {'|', '¦', '┆', '┊'},
+    filetype_exclude = {
+        'help', 'startify', 'alpha', 'dashboard', 'packer', 'neogitstatus',
+        'NvimTree'
+    },
+    buftype_exclude = {'terminal'},
+    use_treesitter = true,
+    show_trailing_blankline_indent = false
+    -- context_char = '┃' -- pecifies the character to be used for the current context indent line
+    -- context_patterns = {'class, function', 'method', '^if'},
 }
 
 -- --------------------------------------------------------------------
 --           end of CONFIGS
 -- --------------------------------------------------------------------
-
 
