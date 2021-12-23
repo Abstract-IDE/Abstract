f3a63beabc36ce328a519943825326e80c25487b
Already told <icanttell@gmail.com>
2021-12-23 19:05:20 +0545

    alpha-nvim.lua: add options

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -17,8 +17,8 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
 local function footer()
-  local datetime = os.date("%I:%M:%p | %d-%m-%Y")
-  return datetime
+	local datetime = os.date("%I:%M:%p | %d-%m-%Y")
+	return datetime
 end
 
 
@@ -36,12 +36,13 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" ,	":ene <BAR> startinsert <CR>"),
-    dashboard.button( "r", "  > Recent"   ,	":Telescope oldfiles<CR>"),
-	dashboard.button( "t", "  > Find text",	":Telescope live_grep <CR>"),
-    dashboard.button( "f", "  > Find file",	":Telescope find_files<CR>"),
-    dashboard.button( "s", "  > Settings" ,	":e ~/.config/nvim/init.lua <CR>"),
-    dashboard.button( "q", "  > Quit NVIM",	":qa<CR>"),
+    dashboard.button( "e", "  > New file" ,	 ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "r", "  > Recent"   ,	 ":Telescope oldfiles<CR>"),
+	dashboard.button( "t", "  > Find text",	 ":Telescope live_grep <CR>"),
+    dashboard.button( "f", "  > Find file",	 ":Telescope find_files<CR>"),
+    dashboard.button( "s", "  > Settings" ,	 ":e ~/.config/nvim/init.lua <CR>"),
+    dashboard.button( "u", "  > Update Plugins",":PackerSync <CR>"),
+    dashboard.button( "q", "  > Quit NVIM",	 ":qa<CR>"),
 }
 
 dashboard.section.footer.val = footer()
