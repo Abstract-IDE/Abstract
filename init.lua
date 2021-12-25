2fc47932f0b438f2f22f254c9d5956fc73a5d540
Already told <icanttell@gmail.com>
2021-12-25 14:45:59 +0545

    init.lua: format code style

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+
 --  .______       ______        _______. __    __  .__   __.  __  ____    ____  __  .___  ___.
 --  |   _  \     /  __  \      /       ||  |  |  | |  \ |  | |  | \   \  /   / |  | |   \/   |
 --  |  |_)  |   |  |  |  |    |   (----`|  |__|  | |   \|  | |  |  \   \/   /  |  | |  \  /  |
@@ -49,11 +50,10 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-        'git', 'clone', 'https://github.com/wbthomason/packer.nvim',
-        install_path
-    })
-    execute 'packadd packer.nvim'
+	fn.system({
+		'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path
+	})
+	execute 'packadd packer.nvim'
 end
 
 -- don't throw any error on first use by packer
@@ -63,204 +63,205 @@ if not ok then return end
 
 
 return packer.startup {
-    function()
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
+	function()
+		-- Packer can manage itself
+		use 'wbthomason/packer.nvim'
 
-        -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
-        -- ━━━━━━━━━━━━━━❰ currently Using ❱━━━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━━━❰ currently Using ❱━━━━━━━━━━━━━━━━ --
 
-        --            Improve Start-UP time
-        use { -- Speed up loading Lua modules in Neovim to improve startup time.
-            'lewis6991/impatient.nvim'
-        }
+		--            Improve Start-UP time
+		use { -- Speed up loading Lua modules in Neovim to improve startup time.
+			'lewis6991/impatient.nvim'
+		}
 
-        use { --  Easily speed up your neovim startup time!. A faster version of filetype.vim
-            'nathom/filetype.nvim'
-        }
+		use { --  Easily speed up your neovim startup time!. A faster version of filetype.vim
+			'nathom/filetype.nvim'
+		}
 
-        use { --  colorscheme for (neo)vim written in lua specially made for roshnivim
-            'shaeinst/roshnivim-cs'
-        }
+		use { --  colorscheme for (neo)vim written in lua specially made for roshnivim
+			'shaeinst/roshnivim-cs'
+		}
 
-        use { -- A collection of common configurations for Neovim's built-in language server client
-            'neovim/nvim-lspconfig',
-            config = [[ require('plugins/lspconfig') ]]
-        }
+		use { -- A collection of common configurations for Neovim's built-in language server client
+			'neovim/nvim-lspconfig',
+			config = [[ require('plugins/lspconfig') ]]
+		}
 
-        use { -- Companion plugin for nvim-lspconfig that allows you to seamlessly install LSP servers locally (inside :echo stdpath("data")).
-            'williamboman/nvim-lsp-installer',
-            config = [[ require('plugins/lsp_installer_nvim') ]]
-        }
+		use { -- Companion plugin for nvim-lspconfig that allows you to seamlessly install LSP servers locally (inside :echo stdpath("data")).
+			'williamboman/nvim-lsp-installer',
+			config = [[ require('plugins/lsp_installer_nvim') ]]
+		}
 
-        use { -- vscode-like pictograms for neovim lsp completion items Topics
-            'onsails/lspkind-nvim',
-            config = [[ require('plugins/lspkind') ]]
-        }
+		use { -- vscode-like pictograms for neovim lsp completion items Topics
+			'onsails/lspkind-nvim',
+			config = [[ require('plugins/lspkind') ]]
+		}
 
-        use { -- Nvim Treesitter configurations and abstraction layer
-            'nvim-treesitter/nvim-treesitter',
+		use { -- Nvim Treesitter configurations and abstraction layer
+			'nvim-treesitter/nvim-treesitter',
 			'nvim-treesitter/playground', -- Treesitter playground integrated into Neovim
-            run = ':TSUpdate',
-            config = [[ require('plugins/treesitter') ]]
-        }
+			run = ':TSUpdate',
+			config = [[ require('plugins/treesitter') ]]
+		}
 
-        use { -- A completion plugin for neovim coded in Lua.
-            'hrsh7th/nvim-cmp',
-            requires = {
-                "hrsh7th/cmp-nvim-lsp",		-- nvim-cmp source for neovim builtin LSP client
-                "hrsh7th/cmp-nvim-lua",		-- nvim-cmp source for nvim lua
-                "hrsh7th/cmp-buffer",		-- nvim-cmp source for buffer words.
-                "hrsh7th/cmp-path",			-- nvim-cmp source for filesystem paths.
-                "hrsh7th/cmp-calc",			-- nvim-cmp source for math calculation.
-                "saadparwaiz1/cmp_luasnip"	-- luasnip completion source for nvim-cmp
-            },
-            config = [[ require('plugins/cmp') ]]
-        }
+		use { -- A completion plugin for neovim coded in Lua.
+			'hrsh7th/nvim-cmp',
+			requires = {
+				"hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim builtin LSP client
+				"hrsh7th/cmp-nvim-lua", -- nvim-cmp source for nvim lua
+				"hrsh7th/cmp-buffer", -- nvim-cmp source for buffer words.
+				"hrsh7th/cmp-path", -- nvim-cmp source for filesystem paths.
+				"hrsh7th/cmp-calc", -- nvim-cmp source for math calculation.
+				"saadparwaiz1/cmp_luasnip" -- luasnip completion source for nvim-cmp
+			},
+			config = [[ require('plugins/cmp') ]]
+		}
 
-        use { -- Snippet Engine for Neovim written in Lua.
-            'L3MON4D3/LuaSnip',
-            requires = {
-                "rafamadriz/friendly-snippets" -- Snippets collection for a set of different programming languages for faster development.
-            },
-            config = [[ require('plugins/luasnip') ]]
-        }
+		use { -- Snippet Engine for Neovim written in Lua.
+			'L3MON4D3/LuaSnip',
+			requires = {
+				"rafamadriz/friendly-snippets" -- Snippets collection for a set of different programming languages for faster development.
+			},
+			config = [[ require('plugins/luasnip') ]]
+		}
 
-        use { -- A super powerful autopairs for Neovim. It support multiple character.
-            'windwp/nvim-autopairs',
-            config = [[ require('plugins/autopairs') ]]
-        }
+		use { -- A super powerful autopairs for Neovim. It support multiple character.
+			'windwp/nvim-autopairs',
+			config = [[ require('plugins/autopairs') ]]
+		}
 
-        use { -- Find, Filter, Preview, Pick. All lua, all the time.
-            'nvim-telescope/telescope.nvim',
-            requires = {
-                {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'},
-                {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'} -- FZF sorter for telescope written in c
-            },
-            config = [[ require('plugins/telescope') ]]
-        }
+		use { -- Find, Filter, Preview, Pick. All lua, all the time.
+			'nvim-telescope/telescope.nvim',
+			requires = {
+				{'nvim-lua/popup.nvim'},
+				{'nvim-lua/plenary.nvim'},
+				{'nvim-telescope/telescope-fzf-native.nvim', run = 'make'} -- FZF sorter for telescope written in c
+			},
+			config = [[ require('plugins/telescope') ]]
+		}
 
-        use { -- Use (neo)vim terminal in the floating/popup window.
-            'voldikss/vim-floaterm',
-            config = [[ require('plugins/floaterm') ]]
-        }
+		use { -- Use (neo)vim terminal in the floating/popup window.
+			'voldikss/vim-floaterm',
+			config = [[ require('plugins/floaterm') ]]
+		}
 
-        use { -- lua `fork` of vim-web-devicons for neovim
-            'kyazdani42/nvim-web-devicons',
-            config = [[ require('plugins/webdevicons_nvim') ]]
-        }
+		use { -- lua `fork` of vim-web-devicons for neovim
+			'kyazdani42/nvim-web-devicons',
+			config = [[ require('plugins/webdevicons_nvim') ]]
+		}
 
-        use { -- Maximizes and restores the current window in Vim
-            'szw/vim-maximizer',
-            config = [[ require('plugins/maximizer') ]]
-        }
+		use { -- Maximizes and restores the current window in Vim
+			'szw/vim-maximizer',
+			config = [[ require('plugins/maximizer') ]]
+		}
 
-        use { -- Smart and powerful comment plugin for neovim. Supports commentstring, dot repeat, left-right/up-down motions, hooks, and more
-            'numToStr/Comment.nvim',
-            config = [[ require('plugins/comment_nvim') ]]
-        }
+		use { -- Smart and powerful comment plugin for neovim. Supports commentstring, dot repeat, left-right/up-down motions, hooks, and more
+			'numToStr/Comment.nvim',
+			config = [[ require('plugins/comment_nvim') ]]
+		}
 
-        use { -- A surround text object plugin for neovim written in lua.
-            'blackcauldron7/surround.nvim',
-            config = [[ require('plugins/surround_nvim') ]]
-        }
+		use { -- A surround text object plugin for neovim written in lua.
+			'blackcauldron7/surround.nvim',
+			config = [[ require('plugins/surround_nvim') ]]
+		}
 
-        use { -- The fastest Neovim colorizer.
-            'norcalli/nvim-colorizer.lua',
-            config = [[ require('plugins/colorizer') ]]
-        }
+		use { -- The fastest Neovim colorizer.
+			'norcalli/nvim-colorizer.lua',
+			config = [[ require('plugins/colorizer') ]]
+		}
 
-        use {
-            'lukas-reineke/indent-blankline.nvim',
-            config = [[ require('plugins/indent_blankline') ]]
-        }
+		use {
+			'lukas-reineke/indent-blankline.nvim',
+			config = [[ require('plugins/indent_blankline') ]]
+		}
 
-        use { -- to change current working directory to project's root directory.
-            'ygm2/rooter.nvim',
-            config = [[ require('plugins/rooter_nvim') ]]
-        }
+		use { -- to change current working directory to project's root directory.
+			'ygm2/rooter.nvim',
+			config = [[ require('plugins/rooter_nvim') ]]
+		}
 
-        use { -- Git signs written in pure lua
-            'lewis6991/gitsigns.nvim',
-            requires = {'nvim-lua/plenary.nvim'},
-            config = [[ require('plugins/gitsigns_nvim') ]]
-        }
+		use { -- Git signs written in pure lua
+			'lewis6991/gitsigns.nvim',
+			requires = {'nvim-lua/plenary.nvim'},
+			config = [[ require('plugins/gitsigns_nvim') ]]
+		}
 
-        use { -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
-            'folke/trouble.nvim',
-            config = [[ require('plugins/trouble_nvim') ]]
-        }
+		use { -- A pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing.
+			'folke/trouble.nvim',
+			config = [[ require('plugins/trouble_nvim') ]]
+		}
 
-        use { -- A snazzy bufferline for Neovim
-            'akinsho/nvim-bufferline.lua',
-            requires = 'kyazdani42/nvim-web-devicons',
-            config = [[ require('plugins/bufferline_nvim') ]]
-        }
+		use { -- A snazzy bufferline for Neovim
+			'akinsho/nvim-bufferline.lua',
+			requires = 'kyazdani42/nvim-web-devicons',
+			config = [[ require('plugins/bufferline_nvim') ]]
+		}
 
-        use { -- A File Explorer For Neovim Written In Lua
-            'kyazdani42/nvim-tree.lua',
-            config = [[ require('plugins/tree_nvim') ]]
-        }
+		use { -- A File Explorer For Neovim Written In Lua
+			'kyazdani42/nvim-tree.lua',
+			config = [[ require('plugins/tree_nvim') ]]
+		}
 
-        use { -- A minimal, stylish and customizable statusline for Neovim written in Lua
-            'Famiu/feline.nvim',
-            -- requires = {
-            --   'nvim-lua/lsp-status.nvim',
-            -- },
-            config = [[ require('plugins/feline_nvim') ]]
-        }
+		use { -- A minimal, stylish and customizable statusline for Neovim written in Lua
+			'Famiu/feline.nvim',
+			-- requires = {
+			--   'nvim-lua/lsp-status.nvim',
+			-- },
+			config = [[ require('plugins/feline_nvim') ]]
+		}
 
-        use { -- No-nonsense floating terminal plugin for neovim
-            "numtostr/FTerm.nvim",
-            config = [[ require('plugins/fterm_nvim') ]]
-        }
+		use { -- No-nonsense floating terminal plugin for neovim
+			"numtostr/FTerm.nvim",
+			config = [[ require('plugins/fterm_nvim') ]]
+		}
 
-        use { -- A format runner for neovim, written in lua.
-            "mhartington/formatter.nvim",
-            config = [[ require('plugins/formatter') ]]
-        }
+		use { -- A format runner for neovim, written in lua.
+			"mhartington/formatter.nvim",
+			config = [[ require('plugins/formatter') ]]
+		}
 
-        use { -- fast and highly customizable greeter for neovim.
-            "goolord/alpha-nvim",
-            requires = {'kyazdani42/nvim-web-devicons'},
-            config = [[ require('plugins/alpha-nvim') ]]
-        }
+		use { -- fast and highly customizable greeter for neovim.
+			"goolord/alpha-nvim",
+			requires = {'kyazdani42/nvim-web-devicons'},
+			config = [[ require('plugins/alpha-nvim') ]]
+		}
 
-        -- ━━━━━━━━━━━━━━━━━❰ DEVELOPMENT ❱━━━━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━━━━━━❰ DEVELOPMENT ❱━━━━━━━━━━━━━━━━━ --
 
-        ----           for flutter/dart
-        use { -- Tools to help create flutter apps in neovim using the native lsp
-            'akinsho/flutter-tools.nvim',
-			ft = { 'dart' },
-            requires = {
-                {'nvim-lua/plenary.nvim'},
+		----           for flutter/dart
+		use { -- Tools to help create flutter apps in neovim using the native lsp
+			'akinsho/flutter-tools.nvim',
+			ft = {'dart'},
+			requires = {
+				{'nvim-lua/plenary.nvim'},
 				{'Neevash/awesome-flutter-snippets'}, -- collection snippets and shortcuts for commonly used Flutter functions and classes
-                {
-                    'dart-lang/dart-vim-plugin', -- Syntax highlighting for Dart in Vim
-                    config = [[ require('plugins/dart_vim_plugin') ]]
-                }
-            },
-            config = [[ require('plugins/fluttertools') ]]
-        }
+				{
+					'dart-lang/dart-vim-plugin', -- Syntax highlighting for Dart in Vim
+					config = [[ require('plugins/dart_vim_plugin') ]]
+				}
+			},
+			config = [[ require('plugins/fluttertools') ]]
+		}
 
-        --            for Web-Development
-        use { --  Use treesitter to auto close and auto rename html tag, work with html,tsx,vue,svelte,php.
-            "windwp/nvim-ts-autotag",
-			ft = { 'html', 'tsx', 'vue', 'svelte', 'php' },
-            requires = {{'nvim-treesitter/nvim-treesitter'}},
-            config = [[ require('plugins/ts-autotag_nvim') ]]
-        }
-
-        -- ━━━━━━━━━━━━━━❰ end of DEVELOPMENT ❱━━━━━━━━━━━━━ --
-
-        -- ━━━━━━━━━━━━❰ end currently Using ❱━━━━━━━━━━━━━━ --
-        -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+		--            for Web-Development
+		use { --  Use treesitter to auto close and auto rename html tag, work with html,tsx,vue,svelte,php.
+			"windwp/nvim-ts-autotag",
+			ft = {'html', 'tsx', 'vue', 'svelte', 'php'},
+			requires = {'nvim-treesitter/nvim-treesitter'},
+			config = [[ require('plugins/ts-autotag_nvim') ]]
+		}
+		-- ━━━━━━━━━━━━━━❰ end of DEVELOPMENT ❱━━━━━━━━━━━━━ --
 
 
-        -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
-        -- ━━━━━━━━━━━━❰ Not currently Using ❱━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━❰ end currently Using ❱━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
-        --[=======[
+
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━❰ Not currently Using ❱━━━━━━━━━━━━━━ --
+
+		--[=======[
 
 		use { -- A light-weight lsp plugin based on neovim built-in lsp with highly a performant UI.
 			'glepnir/lspsaga.nvim',
@@ -310,23 +311,24 @@ return packer.startup {
 
 		--]=======]
 
-        -- ━━━━━━━━━━━━━━━❰ end Not Using ❱━━━━━━━━━━━━━━━━━ --
-        -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
-    end,
+		-- ━━━━━━━━━━━━━━━❰ end Not Using ❱━━━━━━━━━━━━━━━━━ --
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+	end,
 
 
-    config = {
-        -- Move to lua dir so impatient.nvim can cache it
-        compile_path = vim.fn.stdpath('config') .. '/plugin/packer_compiled.lua',
+	config = {
+		-- Move to lua dir so impatient.nvim can cache it
+		compile_path = vim.fn.stdpath('config') .. '/plugin/packer_compiled.lua',
 
 		display = {
 			open_fn = function()
-				return require('packer.util').float({ border = 'single' })
+				return require('packer.util').float({border = 'single'})
 			end
 		}
-    }
+	}
 }
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━❰ end of Plugin Manager ❱━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+
