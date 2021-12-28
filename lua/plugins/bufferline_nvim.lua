257956941b881fbc37e3f73aa1b0dad3675c81c1
Already told <icanttell@gmail.com>
2021-12-28 18:01:46 +0545

    bufferline_nvim.lua: highlight through colorscheme
    now highlight depends on colorscheme instead of hardcoding.
    PS: code style formatted

--- lua/plugins/bufferline_nvim.lua
+++ lua/plugins/bufferline_nvim.lua
@@ -1,3 +1,4 @@
+
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ───────────────────────────────────────────────── --
 --   Plugin:    nvim-bufferline.lua
@@ -12,75 +13,75 @@
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━❰ configs ❱━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+
 require('bufferline').setup {
-    options = {
-        numbers = "ordinal",					-- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string
-        always_show_bufferline = false, 		-- don't show bufferline if there is only one file is opened
+	options = {
+		numbers = "ordinal", -- "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string
+		always_show_bufferline = false, -- don't show bufferline if there is only one file is opened
 
-        --- @deprecated, please specify numbers as a function to customize the styling
-        -- number_style  = "ssubscript",		-- options -> "superscript" | "" | { "none", "subscript" }, buffer_id at index 1, ordinal at index 2
-        close_command		= "bdelete! %d",	-- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d",	-- can be a string | function, see "Mouse actions"
-        left_mouse_command	= "buffer %d",		-- can be a string | function, see "Mouse actions"
-        middle_mouse_command= nil,				-- can be a string | function, see "Mouse actions"
+		close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+		right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
 
-        -- NOTE: this plugin is designed with this icon in mind,
-        -- and so changing this is NOT recommended, this is intended
-        -- as an escape hatch for people who cannot bear it for whatever reason
-        indicator_icon		= '▎',
-        -- indicator_icon      = '',
-        buffer_close_icon	= '',
-        modified_icon		= '●',
-        close_icon			= '',
-        left_trunc_marker	= '',
-        right_trunc_marker	= '',
+		-- NOTE: this plugin is designed with this icon in mind,
+		-- and so changing this is NOT recommended, this is intended
+		-- as an escape hatch for people who cannot bear it for whatever reason
+		indicator_icon = '▎',
+		-- indicator_icon      = '',
+		buffer_close_icon = '',
+		modified_icon = '●',
+		close_icon = '',
+		left_trunc_marker = '',
+		right_trunc_marker = '',
 
-        --- name_formatter can be used to change the buffer's label in the bufferline.
-        --- Please note some names can/will break the
-        --- bufferline so use this at your discretion knowing that it has
-        --- some limitations that will *NOT* be fixed.
-        name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-            -- remove extension from markdown files for example
-            if buf.name:match('%.md') then
-                return vim.fn.fnamemodify(buf.name, ':t:r')
-            end
-        end,
+		--- name_formatter can be used to change the buffer's label in the bufferline.
+		--- Please note some names can/will break the
+		--- bufferline so use this at your discretion knowing that it has
+		--- some limitations that will *NOT* be fixed.
+		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+			-- remove extension from markdown files for example
+			if buf.name:match('%.md') then return vim.fn.fnamemodify(buf.name, ':t:r') end
+		end,
 
-        max_name_length		= 18,
-        max_prefix_length	= 15,	-- prefix used when a buffer is de-duplicate
-        tab_size			= 18,
+		max_name_length = 18,
+		max_prefix_length = 15, -- prefix used when a buffer is de-duplicate
+		tab_size = 18,
 
-        show_close_icon		= false,
-        show_buffer_icons	= true, -- disable filetype icons for buffers
-        show_buffer_close_icons = false,
-        show_tab_indicators = false,
+		show_close_icon = false,
+		show_buffer_icons = true, -- disable filetype icons for buffers
+		show_buffer_close_icons = false,
+		show_tab_indicators = false,
 
-        view = "multiwindow",
-        -- can also be a table containing 2 custom separators
-        -- [focused and unfocused]. eg: { '|', '|' }
-        separator_style = {"", ""}, -- options "slant" | "thick" | "thin" | { 'any', 'any' },
-        offsets = {
-		-- options function , text_" "h always_show_bufferline = false
-            {
-				filetype = "NvimTree",
-				text = "Explorer",
-				text_align = "left"
-			}
-        }
-    },
+		view = "multiwindow",
+		-- can also be a table containing 2 custom separators
+		-- [focused and unfocused]. eg: { '|', '|' }
+		separator_style = {"", ""}, -- options "slant" | "thick" | "thin" | { 'any', 'any' },
+		offsets = {
+			-- options function , text_" "h always_show_bufferline = false
+			{filetype = "NvimTree", text = "Explorer", text_align = "left"},
+		},
+	},
 
-    highlights = {
+	highlights = {
+		fill = {guibg = {highlight = "BufferLineFill", attribute = "bg"}},
+		background = { -- current tab
+			guifg = {highlight = "BufferCurrent", attribute = "fg"},
+			guibg = {highlight = "BufferCurrent", attribute = "bg"},
+		},
+		separator = {guifg = {highlight = "BufferSeparator", attribute = "fg"}},
+		separator_selected = {
+			guifg = {highlight = "BufferSeparatorSelected", attribute = "fg"},
+		},
+		close_button_selected = {
+			guifg = {highlight = "BufferCloseButtonSelected", attribute = "fg"},
+			guibg = {highlight = "BufferCloseButtonSelected", attribute = "bg"},
+		},
+	},
 
-        fill				  = {guibg = "#21252d"},
-        background			  = {guifg = '#FFFFFF', guibg = "#21252d"},
-        separator			  = {guifg = "#141414"},
-        separator_selected	  = {guifg = "#060606"},
-        close_button_selected = {guibg = "#21252d", guifg = "#F1252d"}
-
-    }
 }
 
-vim.cmd( "autocmd BufDelete * if len(getbufinfo({'buflisted':1})) -1 < 1 | set showtabline=1 | endif")
+vim.cmd("autocmd BufDelete * if len(getbufinfo({'buflisted':1})) -1 < 1 | set showtabline=1 | endif")
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━ --
@@ -90,7 +91,7 @@ vim.cmd( "autocmd BufDelete * if len(getbufinfo({'buflisted':1})) -1 < 1 | set s
 
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
--- ━━━━━━━━━━━━━━━━━━━❰ Mappings ❱━━━━━━━━━━━━━━━━━━━ --
+-- ━━━━━━━━━━━━━━━━━━❰ Mappings ❱━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
 local keymap = vim.api.nvim_set_keymap
