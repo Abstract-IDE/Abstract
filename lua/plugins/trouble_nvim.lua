a5d5b058c2bd4e028167553bc3c6bee7ad25b499
Ali Shahid <icanttell@gmail.com>
2021-12-31 19:19:04 +0545

    trouble_nvim.lua: enable mappings in trouble mode

--- lua/plugins/trouble_nvim.lua
+++ lua/plugins/trouble_nvim.lua
@@ -22,29 +22,6 @@ require("trouble").setup {
 	mode = "workspace_diagnostics", -- lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
 	fold_open = "", -- icon used for open folds
 	fold_closed = "", -- icon used for closed folds
-	--[[
-    action_keys = { -- key mappings for actions in the trouble list
-        -- map to {} to remove a mapping, for example:
-        -- close = {},
-        close           = "q", -- close the list
-        cancel          = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-        refresh         = "r", -- manually refresh
-        jump            = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
-        open_split      = { "<c-x>" }, -- open buffer in new split
-        open_vsplit     = { "<c-v>" }, -- open buffer in new vsplit
-        open_tab        = { "<c-t>" }, -- open buffer in new tab
-        jump_close      = {"o"}, -- jump to the diagnostic and close the list
-        toggle_mode     = "m", -- toggle between "workspace" and "document" diagnostics mode
-        toggle_preview  = "P", -- toggle auto_preview
-        hover           = "K", -- opens a small poup with the full multiline message
-        preview         = "p", -- preview the diagnostic location
-        close_folds     = {"zM", "zm"}, -- close all folds
-        open_folds      = {"zR", "zr"}, -- open all folds
-        toggle_fold     = {"zA", "za"}, -- toggle fold of current file
-        previous        = "k", -- preview item
-        next            = "j" -- next item
-    },
-]]
 
 	indent_lines = true, -- add an indent guide below the fold icons
 	auto_open = false, -- automatically open the list when you have diagnostics
@@ -60,7 +37,6 @@ require("trouble").setup {
 		other = "﫠",
 	},
 	use_lsp_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
-}
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━ --
@@ -73,12 +49,35 @@ require("trouble").setup {
 -- ━━━━━━━━━━━━━━━━━━━❰ Mappings ❱━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
---[[
-local keymap = vim.api.nvim_set_keymap
+	action_keys = { -- key mappings for actions in the trouble list
+		-- map to {} to remove a mapping, for example:
+		-- close = {},
+		close = "q", -- close the list
+		cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+		refresh = "r", -- manually refresh
+		jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+		open_split = {"<c-x>"}, -- open buffer in new split
+		open_vsplit = {"<c-v>"}, -- open buffer in new vsplit
+		open_tab = {"<c-t>"}, -- open buffer in new tab
+		jump_close = {"o"}, -- jump to the diagnostic and close the list
+		toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+		toggle_preview = "P", -- toggle auto_preview
+		hover = "K", -- opens a small poup with the full multiline message
+		preview = "p", -- preview the diagnostic location
+		close_folds = {"zM", "zm"}, -- close all folds
+		open_folds = {"zR", "zr"}, -- open all folds
+		toggle_fold = {"zA", "za"}, -- toggle fold of current file
+		previous = "k", -- preview item
+		next = "j", -- next item
+	},
+}
 
-keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
-  {silent = true, noremap = true}
-)
+local keymap = vim.api.nvim_set_keymap
+local opts =  {silent = true, noremap = true}
+
+keymap("n", "gt", "<cmd>TroubleToggle<cr>", opts)
+
+--[===[
 keymap("n", "<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>",
   {silent = true, noremap = true}
 )
@@ -94,7 +93,7 @@ keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
 keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
   {silent = true, noremap = true}
 )
-]]
+--]===]
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━❰ end Mappings ❱━━━━━━━━━━━━━━━━ --
