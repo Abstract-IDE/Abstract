70ae5618e3dd24d835af3c88ee44a40ba5422985
Ali Shahid <icanttell@gmail.com>
2022-01-08 21:13:23 +0545

    nvim-autopairs: fix confliction with cmp

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -24,7 +24,11 @@ require('nvim-autopairs').setup({
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+-- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
+cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
+
 local Rule = require('nvim-autopairs.rule')
 local npairs = require('nvim-autopairs')
 
