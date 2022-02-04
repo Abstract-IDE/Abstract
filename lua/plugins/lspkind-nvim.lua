684e82def3259a6308a36a2d496c4d6fe455112b
Ali Shahid <icanttell@gmail.com>
2022-02-05 00:02:07 +0545

    lspkind-nvim: fix deprecation

--- lua/plugins/lspkind-nvim.lua
+++ lua/plugins/lspkind-nvim.lua
@@ -17,7 +17,7 @@
 
 require('lspkind').init({
 	-- enables text annotations (default: true)
-	with_text = true,
+	mode = true,
 
 	-- enables text annotations (default: 'default')
 	-- default symbol map can be either 'default' or 'codicons' for codicon preset (requires vscode-codicons font installed)
