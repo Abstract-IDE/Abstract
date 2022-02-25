79e46b7c4bfb29e73ea276051002ba68cd9393b3
Ali Shahid <icanttell@gmail.com>
2022-02-25 23:46:23 +0545

    [javascript.lua] expand tab to space

--- lua/langs/javascript.lua
+++ lua/langs/javascript.lua
@@ -8,4 +8,5 @@ local set = vim.opt -- global options
 set.softtabstop = 2
 set.shiftwidth = 2 -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
 set.tabstop = 2 -- spaces per tab
+set.expandtab = true -- expand tabs into spaces
 
