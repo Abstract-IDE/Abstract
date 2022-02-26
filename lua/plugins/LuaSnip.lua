4794631e1a59b9270d250f5e08067c493cb2bb13
Ali Shahid <icanttell@gmail.com>
2022-02-27 01:50:04 +0545

    [LuaSnip, friendly-snippets] add support for react

--- lua/plugins/LuaSnip.lua
+++ lua/plugins/LuaSnip.lua
@@ -60,6 +60,23 @@ require'luasnip/loaders/from_vscode'.lazy_load({
 	paths = {"~/.config/nvim/extra/snippets"},
 })
 
+
+-- ───────────────────────────────────────────────── --
+-- firendl-snippets
+
+local filetype = vim.bo.filetype
+
+if filetype == "javascriptreact" then
+	require("luasnip/loaders/from_vscode").load({include = {"javascriptreact"}})
+	return
+end
+if filetype == "typescriptreact" then
+	require("luasnip/loaders/from_vscode").load({include = {"typescriptreact"}})
+	return
+end
+-- ───────────────────────────────────────────────── --
+
+
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
