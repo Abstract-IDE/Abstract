215fda745f9df2c256b35e681eaf1b2fb0b87b3a
Ali Shahid <icanttell@gmail.com>
2022-03-26 21:10:19 +0545

    [telescope] set override_generic_sorter to true

--- lua/plugins/telescope_nvim.lua
+++ lua/plugins/telescope_nvim.lua
@@ -38,8 +38,8 @@ require('telescope').setup {
 			},
 			vertical = { mirror = false, },
 
-            width = 0.8,
-            height = 0.9,
+			width = 0.8,
+			height = 0.9,
 			preview_width = 0.4,
             preview_cutoff = 80,
 		},
@@ -73,7 +73,7 @@ require('telescope').setup {
 
 		fzf = {
 			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = false, -- override the generic sorter
+			override_generic_sorter = true, -- override the generic sorter
 			override_file_sorter = true, -- override the file sorter
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case". the default case_mode is "smart_case"
 		},
