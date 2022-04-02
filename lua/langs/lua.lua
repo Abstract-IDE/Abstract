035ca7e127e6280d53d254fc9674a11e384b5aea
Ali Shahid <icanttell@gmail.com>
2022-04-02 23:41:01 +0545

    remove langs/ directory

--- lua/langs/lua.lua
+++
@@ -1,8 +0,0 @@
-
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4 -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
-vim.opt.tabstop = 4 -- spaces per tab
-vim.opt.smarttab = true -- <tab>/<BS> indent/dedent in leading whitespace
-vim.opt.autoindent = true -- maintain indent of current line
-vim.opt.expandtab = false -- don't expand tabs into spaces
-
