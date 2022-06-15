1d43c5c0fd0c3f5fde7d69ee27829aed7f5db8b9
Ali Shahid <canttell@gmail>
2022-06-15 18:49:43 +0545

    [aplha-nvim.lua] modify info display

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -19,7 +19,8 @@ local dashboard = require("alpha.themes.dashboard")
 
 local function footer()
 	local datetime = os.date("%I:%M:%p | %d-%m-%Y")
-	return datetime
+	local nvim_version = os.capture("nvim --version | awk 'NR == 1'")
+	return nvim_version .. ' | ' .. datetime
 end
 
 -- Set header
@@ -35,14 +36,15 @@ dashboard.section.header.val = {
 }
 
 -- Set menu
+local options = {}
 dashboard.section.buttons.val = {
-	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-	dashboard.button("t", "  > Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
-	dashboard.button("s", "  > Settings", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("u", "  > Update Plugins", ":PackerSync <CR>"),
-	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>", options),
+	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>", options),
+	dashboard.button("t", "  > Find text", ":Telescope live_grep <CR>", options),
+	dashboard.button("f", "  > Find file", ":Telescope find_files<CR>", options),
+	dashboard.button("s", "  > Settings", ":e ~/.config/nvim/init.lua <CR>", options),
+	dashboard.button("u", "  > Update Plugins", ":PackerSync <CR>", options),
+	dashboard.button("q", "  > Quit NVIM", ":qa<CR>", options),
 }
 
 dashboard.section.footer.val = footer()
