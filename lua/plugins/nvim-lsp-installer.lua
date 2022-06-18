e888f55715951d741b996023c684091c75889c4a
Ali Shahid <canttell@gmail>
2022-06-19 01:22:40 +0545

    [nvim-lsp-installer] add options

--- lua/plugins/nvim-lsp-installer.lua
+++ lua/plugins/nvim-lsp-installer.lua
@@ -19,6 +19,20 @@ if not lspinstaller_imported then return end
 
 
 lspinstaller.setup{
+
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer", "sumneko_lua" }
+    -- This setting has no relation with the `automatic_installation` setting.
+    ensure_installed = {},
+
+    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
+    -- Can either be:
+    --   - false: Servers are not automatically installed.
+    --   - true: All servers set up via lspconfig are automatically installed.
+    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    automatic_installation = false,
+
 	ui = {
 		-- Whether to automatically check for outdated servers when opening the UI window.
 		check_outdated_servers_on_open = true,
@@ -47,16 +61,16 @@ lspinstaller.setup{
         },
 	},
 
-    -- The directory in which to install all servers.
-    -- install_root_dir = path.concat { vim.fn.stdpath "data", "lsp_servers" },
+	-- The directory in which to install all servers.
+	-- install_root_dir = path.concat { vim.fn.stdpath "data", "lsp_servers" },
 
-    -- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
-    -- debugging issues with server installations.
-    log_level = vim.log.levels.INFO,
+	-- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
+	-- debugging issues with server installations.
+	log_level = vim.log.levels.INFO,
 
-    -- Limit for the maximum amount of servers to be installed at the same time. Once this limit is reached, any further
-    -- servers that are requested to be installed will be put in a queue.
-    max_concurrent_installers = 4,
+	-- Limit for the maximum amount of servers to be installed at the same time. Once this limit is reached, any further
+	-- servers that are requested to be installed will be put in a queue.
+	max_concurrent_installers = 4,
 }
 
 
