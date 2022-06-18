db37062793a1c7bf288554e9c77a136d1a52d6b0
Ali Shahid <canttell@gmail>
2022-06-19 01:42:19 +0545

    [nvim-lspconfig] add refrence about window border

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -16,6 +16,18 @@
 
 local M = {}
 
+-- -- change float window border
+-- -- [https://github.com/williamboman/nvim-lsp-installer/issues/475#issuecomment-1037222808]
+-- -- [NOTE: this setting can consume upto 20ms startup time]
+-- local lspconfig_window = require("lspconfig.ui.windows")
+-- local old_defaults = lspconfig_window.default_opts
+--
+-- function lspconfig_window.default_opts(opts)
+--     local win_opts = old_defaults(opts)
+--     win_opts.border = "rounded"
+--     return win_opts
+-- end
+
 -- options for lsp diagnostic
 -- ───────────────────────────────────────────────── --
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
