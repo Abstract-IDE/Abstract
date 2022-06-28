f2f379b0a0a6eb52d9c17f59adf37c3f918dcc34
Ali Shahid <canttell@gmail>
2022-06-28 23:12:47 +0545

    [init.lua] refactor

--- init.lua
+++ init.lua
@@ -29,12 +29,11 @@ vim.g.maplocalleader = '|'
 
 -- plugin config to improve start-up time.
 -- it should be always on top of init.lua file
-require('plugins/impatient_nvim') -- impatient needs to be setup before any other lua plugin is loaded so it is recommended you add the following near the start of your
-require('plugins/filetype_nvim')  -- Easily speed up your neovim startup time!
-
-require('configs')  -- plugin independent configs
-require('autocmd')  -- automatic commands
-require('mappings') -- plugin independent mappings
+require('plugins/impatient_nvim')   -- impatient needs to be setup before any other lua plugin is loaded so it is recommended you add the following near the start of your
+require('plugins/filetype_nvim')    -- Easily speed up your neovim startup time!
+require('configs')                  -- plugin independent configs
+require('autocmd')                  -- automatic commands
+require('mappings')                 -- plugin independent mappings
 require('customs/override_defalut') -- always put this config(override_defalut) at last because it's use to overide the any already defined config
 
 
