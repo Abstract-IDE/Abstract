1b05b3a05db1c71f26bbda7f6ae41b7727e0e1f6
mingang.he <dustgle@gmail.com>
2022-07-09 18:48:38 +0800

    Fix lsp_options for lspconfig[server.name].setup(lsp_options), close #19

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -170,10 +170,11 @@ local function setup_lsp()
 	lspconfig["dartls"].setup(lsp_options)
 
 	for _, server in ipairs(installed_servers) do
+		local opt = {}
 
 		-- for lua
 		if server.name == "sumneko_lua" then
-			lsp_options.settings = {
+			opt.settings = {
 				Lua = {
 					diagnostics = {
 						-- Get the language server to recognize the 'vim', 'use' global
@@ -193,12 +194,12 @@ local function setup_lsp()
 		-- [https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428]
 		if server.name == "clangd" then
 			capabilities.offsetEncoding = { "utf-16" }
-			lsp_options.capabilities = capabilities
+			opt.capabilities = capabilities
 		end
 
 		-- for html
 		if server.name == "html" then
-			lsp_options.filetypes = {"html", "htmldjango"}
+			opt.filetypes = {"html", "htmldjango"}
 		end
 
 		-- for css / scss / sass
@@ -212,10 +213,10 @@ local function setup_lsp()
 					https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/cssls.lua
 			--]==]
 			capabilities.textDocument.completion.completionItem.snippetSupport = true
-			lsp_options.capabilities = capabilities
+			opt.capabilities = capabilities
 		end
 
-		lspconfig[server.name].setup(lsp_options)
+		lspconfig[server.name].setup(vim.tbl_deep_extend('force', lsp_options, opt))
 	end
 end
 
@@ -238,4 +239,3 @@ setup_lsp() -- setup lsp (like pyright, ccls ...)
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
-
