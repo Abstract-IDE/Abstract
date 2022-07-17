a824cc50cea866591da858b9a6085fec0bee2011
Ali Shahid <canttell@gmail>
2022-07-17 09:23:38 +0545

    [mappings.lua] remap dd with smart deletion

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -96,6 +96,17 @@ keymap('t', '<Esc>', '<c-\\><c-n>', options)
 keymap('v', 'J', ":m '>+1<CR>gv=gv", options)
 keymap('v', 'K', ":m '<-2<CR>gv=gv", options)
 
+-- smart deletion, dd
+-- It solves the issue, where you want to delete empty line, but dd will override you last yank.
+-- Code above will check if u are deleting empty line, if so - use black hole register.
+-- [src: https://www.reddit.com/r/neovim/comments/w0jzzv/comment/igfjx5y/?utm_source=share&utm_medium=web2x&context=3]
+local function smart_dd()
+	if vim.api.nvim_get_current_line():match("^%s*$") then
+		return "\"_dd"
+	else return "dd" end
+end
+vim.keymap.set("n", "dd", smart_dd, { noremap = true, expr = true })
+
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━❰ end of Plugin Mapping ❱━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
