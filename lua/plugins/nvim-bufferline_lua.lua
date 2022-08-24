60ab38920070e0b2e2f5cb5dcc2bef9a8e705dd2
Ali Shahid <shaeinst@gmail.com>
2022-08-24 23:32:17 +0545

    [nvim-bufferline.lua] fix deprecation

--- lua/plugins/nvim-bufferline_lua.lua
+++ lua/plugins/nvim-bufferline_lua.lua
@@ -34,8 +34,10 @@ bufferline.setup {
 		-- NOTE: this plugin is designed with this icon in mind,
 		-- and so changing this is NOT recommended, this is intended
 		-- as an escape hatch for people who cannot bear it for whatever reason
-		indicator_icon = '▎',
-		-- indicator_icon      = '',
+		indicator = {
+			style = 'icon',
+			icon = '▎',
+		},
 		buffer_close_icon = '',
 		modified_icon = '●',
 		close_icon = '',
