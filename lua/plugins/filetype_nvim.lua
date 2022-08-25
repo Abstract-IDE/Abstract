90828818b2e975ca97b7416123aef1fe17fa5691
Ali Shahid <shaeinst@gmail.com>
2022-08-25 12:39:30 +0545

    [filetype.nvim] remove unrequired configs

--- lua/plugins/filetype_nvim.lua
+++ lua/plugins/filetype_nvim.lua
@@ -14,13 +14,7 @@
 -- ━━━━━━━━━━━━━━━━━━━❰ configs ❱━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 
-local filetype_ok, _ = pcall(require, "filetype")
-if not filetype_ok then
-	return
-end
-
--- Do not source the default filetype.vim
-vim.g.did_load_filetypes = 1
+pcall(require, "filetype")
 
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
 -- ━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━ --
