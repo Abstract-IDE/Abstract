f39aef720d100e28235b67dfb7aaf303a3b791fb
Ali Shahid <shaeinst@gmail.com>
2022-09-05 13:04:36 +0545

    [nvim-colorizer.lua] disable Name color code in other files

--- lua/plugins/nvim-colorizer_lua.lua
+++ lua/plugins/nvim-colorizer_lua.lua
@@ -24,12 +24,12 @@ colorizer.setup {
 		-- RGB = true,       -- #RGB hex codes
 		-- RRGGBB = true,    -- #RRGGBB hex codes
 		names = false,    -- "Name" codes like Blue or blue
-		-- RRGGBBAA = false, -- #RRGGBBAA hex codes
-		-- AARRGGBB = false, -- 0xAARRGGBB hex codes
-		-- rgb_fn = false,   -- CSS rgb() and rgba() functions
-		-- hsl_fn = false,   -- CSS hsl() and hsla() functions
-		css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-		css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		RRGGBBAA = true, -- #RRGGBBAA hex codes
+		AARRGGBB = true, -- 0xAARRGGBB hex codes
+		rgb_fn = true,   -- CSS rgb() and rgba() functions
+		hsl_fn = true,   -- CSS hsl() and hsla() functions
+		-- css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		-- css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
 		-- Available modes for `mode`: foreground, background,  virtualtext
 		mode = "background", -- Set the display mode.
 		virtualtext = "■",
