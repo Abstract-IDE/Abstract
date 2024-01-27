891d1bfdaef1075dcebbbaa994ccfa2369ed0605
Ali Shahid <shaeinst@gmail.com>
2024-01-27 11:49:45 +0545

    [indent-blankline.nvim] remove hardcoded hightlight

--- lua/plugins/indent-blankline_nvim.lua
+++ lua/plugins/indent-blankline_nvim.lua
@@ -1,41 +1,31 @@
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+─────────────────────────────────────────────────
+Plugin: indent-blankline.nvim
+Github: https://github.com/lukas-reineke/indent-blankline.nvim
 
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
--- ───────────────────────────────────────────────── --
---   Plugin:    indent-blankline.nvim
---   Github:    github.com/lukas-reineke/indent-blankline.nvim
--- ───────────────────────────────────────────────── --
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
-
-
-
-
-
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
--- ━━━━━━━━━━━━━━━━━━━❰ configs ❱━━━━━━━━━━━━━━━━━━━ --
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
-
---[===[
-    NOTE:
-        to know more about filetype_exclude and buftype_exclude
-        https://github.com/lukas-reineke/indent-blankline.nvim/issues/284
-		:set buftype
---]===]
+This plugin adds indentation guides to Neovim. It uses Neovim's virtual text feature and no conceal
+NOTE:
+	to know more about filetype_exclude and buftype_exclude
+	https://github.com/lukas-reineke/indent-blankline.nvim/issues/284
+	:set buftype
+─────────────────────────────────────────────────
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
 
 local _ibl, ibl = pcall(require, "ibl")
-if not _ibl then return end
+if not _ibl then
+	return
+end
 
-ibl.setup {
+ibl.setup({
 
 	debounce = 100,
 	indent = {
 		char = "▎", -- | ¦ ┆ ┊ ║ ▎
-		tab_char= "▎",
+		tab_char = "▎",
 		smart_indent_cap = true,
 		priority = 2,
-		highlight = {
-			"IblIndent1", "IblIndent2", "IblIndent3", "IblIndent4",
-			"IblIndent5", "IblIndent6", "IblIndent7", "IblIndent8",
-		},
 	},
 	whitespace = {
 		highlight = { "Whitespace", "NonText" },
@@ -46,14 +36,23 @@ ibl.setup {
 	},
 	exclude = {
 		filetypes = {
-			"rust", "lspinfo", "packer", "checkhealth", "help", "man", "gitcommit", "TelescopePrompt",
-			"TelescopeResults", 'startify', 'alpha', 'dashboard', 'neogitstatus', 'NvimTree', '',
+			"rust",
+			"lspinfo",
+			"packer",
+			"checkhealth",
+			"help",
+			"man",
+			"gitcommit",
+			"TelescopePrompt",
+			"TelescopeResults",
+			"startify",
+			"alpha",
+			"dashboard",
+			"neogitstatus",
+			"NvimTree",
+			"",
 		},
-		buftypes = { "terminal",'nofile', },
-	}
-}
-
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
--- ━━━━━━━━━━━━━━━━━❰ end configs ❱━━━━━━━━━━━━━━━━━ --
--- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ --
+		buftypes = { "terminal", "nofile" },
+	},
+})
 
