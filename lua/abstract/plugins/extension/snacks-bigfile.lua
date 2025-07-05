89c70b29df17f43e35be5b3f7f977bea9d779a5c
Ali Shahid <shaeinst@gmail.com>
2025-07-06 02:41:28 +0400

    [plugin/snacks] minor update on bigfile plugin

--- lua/abstract/plugins/extension/snacks-bigfile.lua
+++ lua/abstract/plugins/extension/snacks-bigfile.lua
@@ -12,18 +12,24 @@ things like LSP and Treesitter attaching to the buffer.
 --]]
 
 ---@class snacks.bigfile.Config
+---@field enabled? boolean
 local config = {
 	enabled = true,
-	notify = true, -- show notification when big file detected
+	notify = true,       -- show notification when big file detected
 	size = 5 * 1024 * 1024, -- 5MB
+	line_length = 1000,  -- average line length (useful for minified files)
 	-- Enable or disable features when big file detected
 	---@param ctx {buf: number, ft:string}
 	setup = function(ctx)
-		vim.cmd([[NoMatchParen]])
+		if vim.fn.exists(":NoMatchParen") ~= 0 then
+			vim.cmd([[NoMatchParen]])
+		end
 		Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
 		vim.b.minianimate_disable = true
 		vim.schedule(function()
-			vim.bo[ctx.buf].syntax = ctx.ft
+			if vim.api.nvim_buf_is_valid(ctx.buf) then
+				vim.bo[ctx.buf].syntax = ctx.ft
+			end
 		end)
 	end,
 }
