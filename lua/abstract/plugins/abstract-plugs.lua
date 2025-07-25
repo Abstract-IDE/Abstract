686f32529baa2e2e4f6ecf91b6f0a19c9e0e0cbc
Ali Shahid <code@AlixShahid.com>
2025-07-25 12:07:16 +0400

    [plugin] forgot to add abstract-plugs

---
+++ lua/abstract/plugins/abstract-plugs.lua
@@ -0,0 +1,25 @@
+--[[
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+─────────────────────────────────────────────────
+Plugin: abstract-plugs.nvim
+Github: https://github.com/Abstract-IDE/abstract-plugs.nvim
+
+collections of neovim plugins
+─────────────────────────────────────────────────
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+--]]
+
+local spec = {
+	"Abstract-IDE/abstract-plugs.nvim",
+}
+
+spec.config = function()
+	local abstract = require('abs')
+	local set_map = require("abstract.utils.map").set_map
+
+
+	require('abstract.plugins.extension.abstract-window').setup(abstract, set_map)
+	require("abstract.plugins.extension.abstract-terminal").setup(abstract, set_map)
+end
+
+return spec
