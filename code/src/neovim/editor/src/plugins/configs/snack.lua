ec4dba8e7e5fd7cdcb4afecae86cbf9eec9be08c
Ali Shahid <code@AlixShahid.com>
2026-02-23 19:05:29 +0400

    feat: update packages and enable snack's indent

--- code/src/neovim/editor/src/plugins/configs/snack.lua
+++ code/src/neovim/editor/src/plugins/configs/snack.lua
@@ -23,6 +23,7 @@ spec.config = function()
         bufdelete = configs.bufdelete(),
         dashboard = configs.dashboard,
         notifier = configs.notifier,
+        indent = configs.indent,
 
         -- NOTE: using fff for now
         -- picker = configs.picker(),
@@ -473,6 +474,61 @@ configs.dashboard = {
         }
     end,
 
+}
+
+-- Indent
+-- https://github.com/folke/snacks.nvim/blob/main/docs/indent.md
+---@class snacks.indent.Config
+configs.indent = {
+    enabled = true,
+    indent = {
+        enabled = true, -- enable indent guides
+        priority = 1,
+        char = "│",
+        only_scope = false,   -- only show indent guides of the scope
+        only_current = false, -- only show indent guides in the current window
+        hl = "SnacksIndent", ---@type string|string[] hl groups for indent guides
+    },
+    ---@class snacks.indent.Scope.Config: snacks.scope.Config
+    scope = {
+        enabled = true, -- enable highlighting the current scope
+        priority = 200,
+        char = "│", -- │ | ¦ ┆ ┊ ║ ▎
+        underline = true, -- underline the start of the scope
+        only_current = false, -- only show scope in the current window
+        hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
+    },
+    chunk = {
+        -- when enabled, scopes will be rendered as chunks, except for the
+        -- top-level scope which will be rendered as a scope.
+        enabled = false,
+        -- only show chunk scopes in the current window
+        only_current = false,
+        priority = 100,
+        hl = "SnacksIndentChunk", ---@type string|string[] hl group for chunk scopes
+        char = {
+            corner_top = "╭", --    ╭   ┌
+            corner_bottom = "╰", -- ╰   └
+            horizontal = "─",
+            vertical = "│",
+            arrow = ">",
+        },
+    },
+
+    ---@class snacks.indent.animate: snacks.animate.Config
+    animate = {
+        enabled = true, -- vim.fn.has("nvim-0.10") == 1,
+        --- * out: animate outwards from the cursor
+        --- * up: animate upwards from the cursor
+        --- * down: animate downwards from the cursor
+        --- * up_down: animate up or down based on the cursor position
+        style = "out",
+        easing = "linear",
+        duration = {
+            step = 20,   -- ms per step
+            total = 300, -- maximum duration
+        },
+    },
 }
 
 return spec
